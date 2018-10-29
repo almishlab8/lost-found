f9c547e60479654c6cb6d8735d804cd63fad5b88
almishlab8 <almishlab8@gmail.com>
2018-10-29 13:59:51 +0300

    edit API URL

--- src/app/apiurls/serverurls.js
+++ src/app/apiurls/serverurls.js
@@ -1,2 +1,2 @@
-//export const apiKey = 'http://lost_and_found.codeforiraq.org/api'
+//export const apiKey = 'http://lost-and-found.codeforiraq.org/api'
 export const apiKey = 'http://127.0.0.1:8000/api'
